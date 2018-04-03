f6df3053dd6451efe8775a1ec6e5771aeba05034
stiflerfang <fangsheng1987@gmail.com>
2018-04-03 18:57:10 +0800

    stifler:update gradle

--- basecommonmodule/src/main/java/com/stifler/basecommonmodule/base/mvp/LiftAllTransformer.java
+++ basecommonmodule/src/main/java/com/stifler/basecommonmodule/base/mvp/LiftAllTransformer.java
@@ -8,13 +8,22 @@ import io.reactivex.annotations.NonNull;
 import io.reactivex.schedulers.Schedulers;
 
 public class LiftAllTransformer implements ObservableTransformer {
-    private static LiftAllTransformer instance;
+//    private static LiftAllTransformer instance;
+
+    private static class LiftAllTransformerHolder{
+        private static LiftAllTransformer liftAllTransformer = new LiftAllTransformer();
+    }
 
     public static LiftAllTransformer getInstance(){
-        if(instance == null){
-            instance = new LiftAllTransformer();
-        }
-        return instance;
+//        if(instance == null){
+//            instance = new LiftAllTransformer();
+//        }
+//        return instance;
+
+        return LiftAllTransformerHolder.liftAllTransformer;
+    }
+
+    private LiftAllTransformer(){
     }
 
     @Override
